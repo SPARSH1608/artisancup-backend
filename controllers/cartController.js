a324bcb0c0e58f375255266414cd32252feef6af
Sparsh <sparshgoelk@gmail.com>
2024-11-14 01:29:07 +0530

    added add to cart remove from cart and get cart funtioanlity

---
+++ controllers/cartController.js
@@ -0,0 +1,59 @@
+import userModel from '../models/userModel.js';
+
+const addToCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    } else {
+      cartData[req.body.itemId] += 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    return res.json({
+      success: true,
+      message: 'Item added to Cart successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: 'Cant add to cart please try again' });
+  }
+};
+const removeFromCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    return res.json({
+      success: true,
+      message: 'Item removed from Cart successfully',
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: 'Cant remove from cart please try again',
+    });
+  }
+};
+const getCart = async (req, res) => {
+  try {
+    let userData = await userModel.findById(req.body.userId);
+    let cartData = await userData.cartData;
+    return res.json({
+      success: true,
+      cartData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: 'Cant get  cart please try again',
+    });
+  }
+};
+
+export { addToCart, removeFromCart, getCart };
--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 import express from 'express';
 import cors from 'cors';
 import { connectDB } from './config/db.js';
-
+import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import 'dotenv/config';
 //app config
 const app = express();
+app.use(cors());
 const PORT = 4000;
+const allowMethods = ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'];
+const allowHeaders = [
+  'Content-Type',
+  'Authorization',
+  'X-Content-Type-Options',
+  'Accept',
+  'X-Requested-With',
+  'Origin',
+  'Access-Control-Request-Method',
+  'Access-Control-Request-Headers',
+];
+
+app.use(
+  cors({
+    credentials: true,
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders:
+      'Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers',
+  })
+);
 
 // middlewares
 app.use(express.json());
-app.use(cors());
 //db connection
 connectDB();
 
+//api end points
+app.use('/images', express.static('uploads'));
+app.use('/api/food', foodRouter);
+app.use('/api/user', userRouter);
+app.use('/api/cart', cartRouter);
 app.get('/', (req, res) => {
   res.send('API');
 });
